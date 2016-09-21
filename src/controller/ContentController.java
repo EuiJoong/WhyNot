f4e91ee1285074900dca18acc58e4521d8e36ac6
EuiJoong <euijki87@gmail.com>
2016-09-21 20:25:46 +0900

    뭐였지?

--- src/controller/ContentController.java
+++ src/controller/ContentController.java
@@ -64,84 +64,81 @@ public class ContentController {
 	
 
 	// -------------- 컨텐츠 -----------------------------------
+	   public VideoDBBean insertVideo(FileItem fileItem) throws Exception {
+		      // 파일 업로드 처리
+		      String fileName = fileItem.getName();
+		      long fileSize = fileItem.getSize() / 1024 + (fileItem.getSize() % 1024 > 0 ? 1 : 0);
+		      // File uploadedFile = new File(fileName);
+		      
+		      String[] fileNameE=fileName.split("[.]");
+		      String ext=fileNameE[fileNameE.length-1];//확장자
+		      String filetitle="";
+		      
+		      if(fileNameE.length>2)
+		      {
+		         for(int i=0;i<fileNameE.length-1;i++)
+		         {
+		            filetitle+=fileNameE[i]+".";                        }
+		      }
+		      
+		      //이제 난수
+		      for(int i=0;i<16;i++){
+		         filetitle+=(int)(Math.random()*9);
+		      }
+		      filetitle+=".";
+		      //filetitle:파일명, ext:파일 확장자
+		      String result=filetitle+ext;
+		      
+		      
+		      //System.out.println("filename : " + fileName);
+		      //System.out.println("filesize : " + fileSize);
+		      Socket client = new Socket("192.168.0.9", 12345);
+		      System.out.println("서버접속성공!!");
+		      InputStream is = fileItem.getInputStream();
+		      OutputStream os = client.getOutputStream();
+		      DataOutputStream dout = new DataOutputStream(os);
 
+		      
+		      dout.writeLong(fileSize);
+		      dout.writeUTF(result);
+		      System.out.println("경로 : " + result);
+		      byte[] buffer = new byte[1024];
 
-	public VideoDBBean insertVideo(FileItem fileItem) throws Exception {
+		      int len;
+		      System.out.println("파일전송중..");
+		      for (; fileSize > 0; fileSize--) {
+		         len = is.read(buffer);
+		         os.write(buffer, 0, len);
+		      }
+		      dout.close();
+		      os.close();
 
-		// 파일 업로드 처리
-		String fileName = fileItem.getName();
-		long fileSize = fileItem.getSize() / 1024 + (fileItem.getSize() % 1024 > 0 ? 1 : 0);
-		// File uploadedFile = new File(fileName);
-		
-		String[] fileNameE=fileName.split("[.]");
-		String ext=fileNameE[fileNameE.length-1];//확장자
-		String filetitle="";
-		
-		if(fileNameE.length>2)
-		{
-			for(int i=0;i<fileNameE.length-1;i++)
-			{
-				filetitle+=fileNameE[i]+".";								}
-		}
-		
-		//이제 난수
-		for(int i=0;i<16;i++){
-			filetitle+=(int)(Math.random()*9);
-		}
-		filetitle+=".";
-		//filetitle:파일명, ext:파일 확장자
-		String result=filetitle+ext;
-		
-		
-		//System.out.println("filename : " + fileName);
-		//System.out.println("filesize : " + fileSize);
-		Socket client = new Socket("192.168.0.9", 12345);
-		System.out.println("서버접속성공!!");
-		InputStream is = fileItem.getInputStream();
-		OutputStream os = client.getOutputStream();
-		DataOutputStream dout = new DataOutputStream(os);
+		      System.out.println("파일전송완료!!");
+		      // return new ModelAndView("../index.jsp");
+		      // 1.인코딩 하고, 파일네임을 arg1에 박아서
+		      // -------------------------------------------
+		      /*
+		       * File source = new File("movie/sample2.mp4"); File target = new
+		       * File("movie/sample22.flv");
+		       */
+		      VideoDBBean v_dto = new VideoDBBean();
+		      // DB에 실제로 저장되는 경로
+		      String filedir = "C:\\Users\\고경숙\\Documents\\WhyNot";
+		      //String[] fileNameE = fileName.split("[.]");// 0: 파일 네임, 1: 파일
+		                                       // 확장자
+		      System.out.println(fileNameE.length);
+		      System.out.println(fileNameE[0]);
+		      System.out.println(fileNameE[1]);
 
-		
-		dout.writeLong(fileSize);
-		dout.writeUTF(result);
-		System.out.println("경로 : " + result);
-		byte[] buffer = new byte[1024];
+		      v_dto.setFilename(filetitle + "mp4");
+		      v_dto.setFiledir(filedir);
+		      v_dto.setMnum(0);
+		      v_dto.setVdnum(0);
+		      System.out.println("여기까지 왔나");
+		      return v_dto;// 임의로! 넣어지나 보자!
+		      // 2.forward로 값 있는 그대로 쭈욱 옮겨서..근데 저 쿼리문은 어떻게 실행하지
 
-		int len;
-		System.out.println("파일전송중..");
-		for (; fileSize > 0; fileSize--) {
-			len = is.read(buffer);
-			os.write(buffer, 0, len);
-		}
-		dout.close();
-		os.close();
-
-		System.out.println("파일전송완료!!");
-		// return new ModelAndView("../index.jsp");
-		// 1.인코딩 하고, 파일네임을 arg1에 박아서
-		// -------------------------------------------
-		/*
-		 * File source = new File("movie/sample2.mp4"); File target = new
-		 * File("movie/sample22.flv");
-		 */
-		VideoDBBean v_dto = new VideoDBBean();
-		// DB에 실제로 저장되는 경로
-		String filedir = "C:\\Users\\고경숙\\Documents\\WhyNot";
-		//String[] fileNameE = fileName.split("[.]");// 0: 파일 네임, 1: 파일
-													// 확장자
-		System.out.println(fileNameE.length);
-		System.out.println(fileNameE[0]);
-		System.out.println(fileNameE[1]);
-
-		v_dto.setFilename(filetitle + "mp4");
-		v_dto.setFiledir(filedir);
-		v_dto.setMnum(0);
-		v_dto.setVdnum(0);
-		System.out.println("여기까지 왔나");
-		return v_dto;// 임의로! 넣어지나 보자!
-		// 2.forward로 값 있는 그대로 쭈욱 옮겨서..근데 저 쿼리문은 어떻게 실행하지
-
-	}
+		   }
 
 	@RequestMapping(value = "/list.content")
 	public ModelAndView listContent(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
@@ -171,115 +168,115 @@ public class ContentController {
 	}
 
 	@RequestMapping(value = "/cont_insertPro.content") // 인강등록Pro(학교)
-	public ModelAndView insertProContent(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
-		System.out.println("ContentController_insertProContent() 실행");
-		// ===================================================================
-		arg0.setCharacterEncoding("UTF-8");
-		arg1.setCharacterEncoding("UTF-8");
-		System.out.println("제목:" + arg0.getParameter("title"));
-		System.out.println("내용:" + arg0.getParameter("content"));
+	 public ModelAndView insertProContent(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
+	      System.out.println("ContentController_insertProContent() 실행");
+	      // ===================================================================
+	      arg0.setCharacterEncoding("UTF-8");
+	      arg1.setCharacterEncoding("UTF-8");
+	      System.out.println("제목:"+arg0.getParameter("title"));
+	      System.out.println("내용:"+arg0.getParameter("content"));
+	      
+	      OnlineContentDBBean oc_dto = null;
+	      PhotoDBBean p_dto = null;
+	      VideoDBBean v_dto = null;
+	      boolean isMultipart = ServletFileUpload.isMultipartContent(arg0);
 
-		OnlineContentDBBean oc_dto = null;
-		PhotoDBBean p_dto = null;
-		VideoDBBean v_dto = null;
-		boolean isMultipart = ServletFileUpload.isMultipartContent(arg0);
+	      if (isMultipart) {
+	         File temporaryDir = new File("C:\\Users\\고경숙\\Documents\\WhyNot");// 동영상
+	                                                      // 임시저장폴더
+	         DiskFileItemFactory factory = new DiskFileItemFactory();
+	         System.out.println("factory 생성성공");
+	         factory.setSizeThreshold(1 * 1024 * 1024);// 메모리 임시저장 제한용량
+	         factory.setRepository(temporaryDir);
+	         ServletFileUpload upload = new ServletFileUpload(factory);
+	         System.out.println("ServletFileUpload 생성성공");
+	         upload.setSizeMax(3 * 1024 * 1024 * 1024);// 파일최대 제한용량
+	         List items = null;
+	         try {
+	            items = upload.parseRequest(arg0);
+	         } catch (FileUploadException fe) {
+	            System.out.println(fe);
+	            fe.printStackTrace();
+	         }
+	         if (items != null) {
+	            Iterator iter = items.iterator();
+	            oc_dto = new OnlineContentDBBean();
+	            while (iter.hasNext()) {
+	               FileItem fileItem = (FileItem) iter.next();
+	               if (fileItem.isFormField()) {
+	                  // 파일을 제외한 나머지 파라미터 처리
+	                  String name = fileItem.getFieldName();
+	                  System.out.println("값일때 : " + name);
+	                  String value = fileItem.getString("UTF-8");
+	                  System.out.println("값일때 : " + value);
+	                  if(value==null || value.equals(""))
+	                     value="0";
+	                  Integer ptnum; // 사진번호
+	                  Integer vdnum; // 동영상번호
+	                  switch (name) {
+	                  case "ctnum":
+	                     oc_dto.setCtnum(Integer.parseInt(value));
+	                     break;
+	                  case "mnum":
+	                     oc_dto.setMnum(Integer.parseInt(value));
+	                     break;
+	                  case "title":
+	                     oc_dto.setTitle(value);
+	                     break;
+	                  case "content":
+	                     oc_dto.setContent(value);
+	                     break;
+	                  case "price":
+	                     oc_dto.setPrice(Integer.parseInt(value));
+	                     break;
+	                  }
 
-		if (isMultipart) {
-			File temporaryDir = new File("C:\\Users\\Administrator\\Desktop\\testvideo\\");// 동영상
-			// 임시저장폴더
-			DiskFileItemFactory factory = new DiskFileItemFactory();
-			System.out.println("factory 생성성공");
-			factory.setSizeThreshold(1 * 1024 * 1024);// 메모리 임시저장 제한용량
-			factory.setRepository(temporaryDir);
-			ServletFileUpload upload = new ServletFileUpload(factory);
-			System.out.println("ServletFileUpload 생성성공");
-			upload.setSizeMax(3 * 1024 * 1024 * 1024);// 파일최대 제한용량
-			List items = null;
-			try {
-				items = upload.parseRequest(arg0);
-			} catch (FileUploadException fe) {
-				System.out.println(fe);
-				fe.printStackTrace();
-			}
-			if (items != null) {
-				Iterator iter = items.iterator();
-				oc_dto = new OnlineContentDBBean();
-				while (iter.hasNext()) {
-					FileItem fileItem = (FileItem) iter.next();
-					if (fileItem.isFormField()) {
-						// 파일을 제외한 나머지 파라미터 처리
-						String name = fileItem.getFieldName();
-						System.out.println("값일때 : " + name);
-						String value = fileItem.getString("UTF-8");
-						System.out.println("값일때 : " + value);
-						if(value==null || value.equals(""))
-							value="0";
-						Integer ptnum; // 사진번호
-						Integer vdnum; // 동영상번호
-						switch (name) {
-						case "ctnum":
-							oc_dto.setCtnum(Integer.parseInt(value));
-							break;
-						case "mnum":
-							oc_dto.setMnum(Integer.parseInt(value));
-							break;
-						case "title":
-							oc_dto.setTitle(value);
-							break;
-						case "content":
-							oc_dto.setContent(value);
-							break;
-						case "price":
-							oc_dto.setPrice(Integer.parseInt(value));
-							break;
-						}
+	               } else {
+	                  // 파일 업로드 처리
+	                  String name = fileItem.getFieldName();
+	                  System.out.println("파일일때 : " + name + "/ " + fileItem.getName());
+	                  switch (name) {
+	                  case "image-file": // image파일 처리
+	                     System.out.println("사진파일일때 : " + name);
+	                     String[] nameArr = fileItem.getName().split("[.]");
+	                     String upPath = "C:\\Users\\고경숙\\Documents\\WhyNot";
+	                     p_dto = new PhotoDBBean();
+	                     p_dto.setFiledir(upPath);
+	                     p_dto.setFilename(nameArr[0]);
+	                     p_dto.setFileext(nameArr[1]);
+	                     File imageFile = new File(upPath + fileItem.getName());
+	                     fileItem.write(imageFile);
+	                     break;
+	                  case "video-file": // video파일 처리
+	                     System.out.println("비디오파일일때 : " + name);
+	                     
+	                     //여기서 난수발생해야 dao에 들어가겠구나!
+	                     
 
-					} else {
-						// 파일 업로드 처리
-						String name = fileItem.getFieldName();
-						System.out.println("파일일때 : " + name + "/ " + fileItem.getName());
-						switch (name) {
-						case "image-file": // image파일 처리
-							System.out.println("사진파일일때 : " + name);
-							String[] nameArr = fileItem.getName().split("[.]");
-							String upPath = "C:\\Users\\고경숙\\Documents\\WhyNot";
-							p_dto = new PhotoDBBean();
-							p_dto.setFiledir(upPath);
-							p_dto.setFilename(nameArr[0]);
-							p_dto.setFileext(nameArr[1]);
-							File imageFile = new File(upPath + fileItem.getName());
-							fileItem.write(imageFile);
-							break;
-						case "video-file": // video파일 처리
-							System.out.println("비디오파일일때 : " + name);
-							
-							//여기서 난수발생해야 dao에 들어가겠구나!
-							
+	                     
+	                     
+	                     if (fileItem.getName() == null || fileItem.getName().equals(""))
+	                        break;
+	                     
+	                     v_dto = insertVideo(fileItem); // video파일 전송 및 인코딩
+	                                             // 메소드 호출
+	                     break;
+	                  }
 
-							
-							
-							if (fileItem.getName() == null || fileItem.getName().equals(""))
-								break;
-							
-							v_dto = insertVideo(fileItem); // video파일 전송 및 인코딩
-															// 메소드 호출
-							break;
-						}
+	               }
+	            }
+	         }
+	      }
+	      if (v_dto == null) {
+	         v_dto = new VideoDBBean();
+	      }
+	      p_dto.setMnum(oc_dto.getMnum());
+	      v_dto.setMnum(oc_dto.getMnum());
+	      onlineContentDAO.insertContent(oc_dto, p_dto, v_dto);
 
-					}
-				}
-			}
-		}
-		if (v_dto == null) {
-			v_dto = new VideoDBBean();
-		}
-		p_dto.setMnum(oc_dto.getMnum());
-		v_dto.setMnum(oc_dto.getMnum());
-		onlineContentDAO.insertContent(oc_dto, p_dto, v_dto);
-
-		// ===================================================================
-		return new ModelAndView("content/contentList.jsp"); // 나중에 마이페이지로???
-	}
+	      // ===================================================================
+	      return new ModelAndView("content/contentList.jsp"); // 나중에 마이페이지로???
+	   }
 
 	@RequestMapping(value = "/cont_detail.content") // 인강 상세보기
 	public ModelAndView detailContent(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
