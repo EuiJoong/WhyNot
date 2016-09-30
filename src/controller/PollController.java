a48a35f5be9cf618d2b02e8894d2a63fc06d0ea4
EuiJoong <euijki87@gmail.com>
2016-09-30 18:56:50 +0900

    no message

--- src/controller/PollController.java
+++ src/controller/PollController.java
@@ -7,28 +7,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-<<<<<<< HEAD
 import category.model.CategoryDBBean;
 import event.poll.model.CurrPollDBBean;
 import event.poll.model.EventPollDAO;
-=======
 import event.poll.model.PollDAO;
 import event.poll.model.PollDBBean;
->>>>>>> 1809d8cb5141a62898c7d342d3b5d4d0ee3e90bc
 
 @Controller
 public class PollController {
 	
 	private PollDAO pollDAO;
+	private EventPollDAO eventPollDAO;
 	
 	public void setPollDAO(PollDAO pollDAO) {
 		this.pollDAO = pollDAO;
+	}
+	
+	public void setEventPollDAO(EventPollDAO eventPollDAO) {
+		this.eventPollDAO = eventPollDAO;
 	}
 
 	@RequestMapping(value = "/list.poll") //최상위(현재 진행중인..?) 투표 현황보기
 	public ModelAndView listPoll(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
 		System.out.println("PollController_listPoll() 실행");
-<<<<<<< HEAD
 		//이거쓰자
 		List list=eventPollDAO.getPoll();
 		ModelAndView mav = new ModelAndView();
@@ -44,8 +45,6 @@ public class PollController {
 	public ModelAndView doPoll(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
 		System.out.println("PollController_doPoll() 실행");
 		
-		return new ModelAndView("");
-=======
 		String para = arg0.getParameter("eventNum");
 		List<PollDBBean> pollList = null;
 		if(para!=null)
@@ -61,6 +60,5 @@ public class PollController {
 		mav.addObject("pollList", pollList);
 		mav.addObject("eventList", eventList);
 		return mav;
->>>>>>> 1809d8cb5141a62898c7d342d3b5d4d0ee3e90bc
 	}
 }
